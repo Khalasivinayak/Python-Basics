22307268a2ba8e7f64f6cec05042c5f9cbef68ef
VINAYAK KHALASI <kvijaykumar@successkpi.com>
2022-08-22 23:23:12 +0530

    First Commit on list

---
+++ prac_1.py
@@ -0,0 +1,4 @@
+#list comprehensions:-
+#List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+#Syntax:-newlist = [expression for item in iterable if condition == True]
